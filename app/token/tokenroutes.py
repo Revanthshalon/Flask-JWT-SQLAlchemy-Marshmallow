2a35a7a1f337a86f737cc3ef3341acbde034062f
Revanthshalon <revanthshalon@gmail.com>
2020-11-06 23:04:29 +0530

    token routes

--- app/token/tokenroutes.py
+++ app/token/tokenroutes.py
@@ -1,7 +1,45 @@
-from flask import request
+from flask import request, jsonify
 from . import token
 from app import db, jwt
+from sqlalchemy import exc
+from app.models.UserModel import UserSchema, User
 
-@token.route("/")
-def index():
-    return "Success"
+us = UserSchema() # Importing User Schema to convert the data 
+
+
+@jwt.user_claims_loader
+def add_claims_to_access_token(user):
+    return user
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user['username']
+
+
+@token.route("/register", methods=['POST'])
+def register():
+    
+    test = User.query.filter_by(email=request.form['email']).first()
+
+    if test:
+        resp = {
+            "message": "Email Already Exists"
+        }
+        return jsonify(resp), 409
+try:
+    user = User(
+        username = request.form['username'],
+        email = request.form['email'],
+        password = request.form['password'],
+        user_role = 1
+    )
+    db.session.add(User)
+    db.session.commit()
+
+    resp = jsonify({"message": "User Registered Successfully"})
+    return resp, 201
+except exc.IntegrityError:
+    db.session.rollback()
+    resp = jsonify({"message": "Database Error"})
+    return resp, 409
