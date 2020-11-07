a9dff67798c55868f14ce9ec00c2eca830dba3e7
Revanthshalon <revanthshalon@gmail.com>
2020-11-07 20:25:25 +0530

    Uploads

--- app/dashboard/dashboardroutes.py
+++ app/dashboard/dashboardroutes.py
@@ -2,10 +2,15 @@ import os
 from flask import request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt_claims
 from werkzeug.utils import secure_filename
+from app.models.UploadModel import UploadModel, UploadModelSchema
+from datetime import datetime
+from app import db
 from . import dashboard
+from sqlalchemy import exc
 
 ALLOWED_EXTENSIONS = ['csv']
 UPLOADS_FOLDER = "D:/Uploads/"
+
 
 def allowed_filename(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
@@ -35,6 +40,11 @@ def upload():
             }
             resp['current_user'] = current_user
             resp['user_claims'] = user_claims
+
+            uid = UploadModel(userid = user_claims['userid'], filename=filename, filelocation=os.path.join(BASE_DIR, filename), upload_date = datetime.now())
+            db.session.add(uid)
+            db.session.commit()
+
             return jsonify(resp), 201
 
         os.makedirs(os.path.join(UPLOADS_FOLDER,current_user))
@@ -48,4 +58,27 @@ def upload():
         resp['current_user'] = current_user
         resp['user_claims'] = user_claims
 
+        uid = UploadModel(userid = user_claims['userid'], filename=filename, filelocation=os.path.join(BASE_DIR, filename), upload_date = datetime.now())
+        try:
+            db.session.add(uid)
+            db.session.commit()
+        except:
+            db.session.rollback()
+            resp = {
+                "message": "Database Error"
+            }
+            return jsonify(resp), 409
+
         return jsonify(resp),200
+
+
+@dashboard.route("/files", methods=["GET"])
+@jwt_required
+def uploadedfiles():
+    current_user = get_jwt_identity()
+    user_claims = get_jwt_claims()
+    upload_data = UploadModel.query.filter_by(userid=user_claims['userid']).all()
+    ums = UploadModelSchema()
+    upload_data = [ums.dump(i) for i in upload_data]
+    resp = jsonify(upload_data)
+    return resp, 200
--- app/models/UploadModel.py
+++ app/models/UploadModel.py
@@ -2,10 +2,20 @@ from app import db, ma
 
 class UploadModel(db.Model):
 
-    __tablename__ = "Uploads"
+    __tablename__ = "uploads"
 
     uploadid = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.Integer, db.ForeignKey('users.userid'), nullable=False)
     filename = db.Column(db.String(100), unique=True, index=True, nullable=False)
     filelocation = db.Column(db.String(255),nullable=False)
+    upload_date = db.Column(db.DateTime, nullable=False)
     
+
+class UploadModelSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = UploadModel
+
+    uploadid = ma.auto_field()
+    filename = ma.auto_field()
+    filelocation = ma.auto_field()
+    upload_date = ma.auto_field()
