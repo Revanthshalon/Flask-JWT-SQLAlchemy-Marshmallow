089acad7454f9e2fa8f489dbc817e660c45d4bc4
Revanthshalon <revanthshalon@gmail.com>
2020-11-07 21:41:09 +0530

    Analytics

--- app/analytics/analyticsroutes.py
+++ app/analytics/analyticsroutes.py
@@ -1,5 +1,24 @@
-from flask import request
+from flask import request, jsonify
 import pandas as pd
+from . import analytics
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt_claims
+from app.models.UploadModel import UploadModel, UploadModelSchema
 
-df = pd.read_csv('D:/Uploads/revanth\\Auto.csv')
-print(df.head())
+
+@analytics.route("/view/<fileid>", methods=['GET'])
+@jwt_required
+def viewdf(fileid):
+    current_user = get_jwt_identity()
+    user_claims = get_jwt_claims()
+    uploaded_file = UploadModel.query.filter_by(uploadid = int(fileid)).first()
+    ums = UploadModelSchema()
+    uploaded_file = ums.dump(uploaded_file)
+    df = pd.read_csv(uploaded_file['filelocation'])
+
+    return jsonify(df.head(5).to_dict(orient='records')), 200
+    
+
+# @analytics.route("/drop/<colname>", methods=['DELETE'])
+# @jwt_required
+# def editdf(colname):
+    
